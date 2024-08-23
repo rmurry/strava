1355e9c225415fc94a4593d844eba23b59b89814
rmurry <rylanmurry@gmail.com>
2024-08-23 09:16:40 -0500

    successfully pulled 30 results

--- source.R
+++ source.R
@@ -1,9 +1,27 @@
 library(dplyr)
-library(rStrava)
 library(httr)
+library(jsonlite)
 
+strava_endpoint <- oauth_endpoint(
+  request = NULL,
+  authorize = "authorize", 
+  access = "token",
+  base_url = "https://www.strava.com/api/v3/oauth/"
+)
 
-stoken <- httr::config(token = strava_oauth(app_name, client_id, client_secret, cache = TRUE))
-my_acts <- get_activity_list(stoken)
-act_data <- compile_activities(my_acts)
-chk_nopolyline(act_data)
+myapp <- oauth_app(
+  "strava", 
+  key = 126618,  # <- change this to your strava ID 
+  secret = "6d4056b400b9042651c93c85fbf5cfbffe6e0694" # <- change this to your client ID
+)
+
+mytok <- oauth2.0_token(
+  endpoint = strava_endpoint, 
+  app = myapp,
+  scope = c("activity:read"), # somehow it does not work with multiple scopes
+  cache = TRUE
+)
+
+resp <- GET("https://www.strava.com/api/v3/athlete/activities", config(token = mytok))
+
+dat <- fromJSON(rawToChar(resp$content))
