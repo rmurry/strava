383d6b3344b56bb78511e743ff4933d412f0cf9a
rmurry <rylanmurry@gmail.com>
2024-08-23 09:56:03 -0500

    increased to last 200 activities

--- source.R
+++ source.R
@@ -1,6 +1,12 @@
 library(dplyr)
 library(httr)
 library(jsonlite)
+library(lubridate)
+library(glue)
+
+beg_of_year <- as.POSIXct('2024-01-01',format = '%Y-%m-%d %HH:%MM:%SS')
+
+as.POSIXct()
 
 strava_endpoint <- oauth_endpoint(
   request = NULL,
@@ -22,6 +28,6 @@ mytok <- oauth2.0_token(
   cache = TRUE
 )
 
-resp <- GET("https://www.strava.com/api/v3/athlete/activities", config(token = mytok),add_headers(per_page = 1000))
+resp <- GET(glue("https://www.strava.com/api/v3/athlete/activities?per_page=200"), config(token = mytok))
 
 dat <- fromJSON(rawToChar(resp$content))
