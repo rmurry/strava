0f64719cd272565f13d1eb97de4fd3d264b9e01c
Rylan <rylanmurry@gmail.com>
2024-08-22 21:27:48 -0500

    removed secrets

--- source.R
+++ source.R
@@ -3,7 +3,7 @@ library(rStrava)
 library(httr)
 
 
-stoken <- httr::config(token = strava_oauth('custom_app', '126618', '6d4056b400b9042651c93c85fbf5cfbffe6e0694', cache = TRUE))
+stoken <- httr::config(token = strava_oauth(app_name, client_id, client_secret, cache = TRUE))
 my_acts <- get_activity_list(stoken)
 act_data <- compile_activities(my_acts)
 chk_nopolyline(act_data)
